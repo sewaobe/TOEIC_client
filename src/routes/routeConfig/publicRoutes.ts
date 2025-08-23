9021ce6462ba793ee5aff6499d6223e06fbb4f96
thanha17 <148538593+thanha17@users.noreply.github.com>
2025-08-23 14:17:00 +0700

    thêm trang home

--- src/routes/routeConfig/publicRoutes.ts
+++ src/routes/routeConfig/publicRoutes.ts
@@ -9,12 +9,16 @@ const LandingPage = lazy(() => import('../../views/pages/LandingPage'));
 const AuthPage = lazy(() => import('../../views/pages/AuthPage'));
 const TestDemoPage = lazy(() => import('../../views/pages/TestDemoPage'));
 const OverviewTestPage = lazy(() => import('../../views/pages/OverviewPage'));
+
+const HomePage = lazy(() => import('../../views/pages/HomePage'));
+
 const publicRoutes: AppRoute[] = [
   { path: '/', element: LandingPage },
-  { path: '/home', element: LandingPage },
+  { path: '/ladingPage', element: LandingPage },
   { path: '/login', element: AuthPage },
   { path: '/overview-test', element: OverviewTestPage },
   { path: '/test-demo', element: TestDemoPage },
+  { path: '/home', element: HomePage },
 ];
 
 export default publicRoutes;

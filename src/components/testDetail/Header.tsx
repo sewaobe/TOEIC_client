ab299a109f072845d14d1d539c3a048b4b014d34
baonp <sewaobe@gmail.com>
2025-10-30 21:37:13 +0700

    Feat: added toast into create comment and setup axiosClient response URL API.

--- src/components/testDetail/Header.tsx
+++ src/components/testDetail/Header.tsx
@@ -1,8 +1,15 @@
-const Header: React.FC = () => {
+import { Link } from "react-router-dom";
+
+interface HeaderProps {
+  testTitle: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ testTitle }) => {
+
   return (
     <div className='flex items-center justify-between mb-4'>
       <nav className='text-sm text-gray-500'>
-        Trang chủ / TOEIC / New Economy Test 1
+        <Link to="/tests" style={{ color: "blue" }}>Trang chủ</Link> / {testTitle}
       </nav>
     </div>
   );

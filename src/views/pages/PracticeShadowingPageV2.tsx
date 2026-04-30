0461475e7bb69b423273ad1012933e3eee190909
baonp <sewaobe@gmail.com>
2026-04-30 12:26:09 +0700

    chore(PracticeShadowingV2): Change folder name to shadowing-detai-v2

--- src/views/pages/PracticeShadowingPageV2.tsx
+++ src/views/pages/PracticeShadowingPageV2.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect, useRef } from 'react';
 import {
   Autorenew as Loader2,
 } from '@mui/icons-material';
-import Sidebar from '../../components/practices/shadowing-v2/Sidebar';
-import RightContent from '../../components/practices/shadowing-v2/RightContent';
+import Sidebar from '../../components/practices/shadowing-detail-v2/Sidebar';
+import RightContent from '../../components/practices/shadowing-detail-v2/RightContent';
 import PracticeLayout from '../layouts/PracticeLayout';
 
 // ==========================================
@@ -14,6 +14,7 @@ interface ShadowingTiming {
   endTime: number;
   startTime: number;
   text: string;
+  ipa: string;
   translationVi: string;
 }
 
@@ -23,6 +24,8 @@ interface ShadowingData {
   level: string;
   part_type: number;
   source_type?: 'audio' | 'youtube';
+  thumbnail: string;
+  tags: string[];
   status: string;
   transcript: string;
   audio_url: string;
@@ -53,6 +56,8 @@ const mockShadowingData: ShadowingData = {
   part_type: 3,
   status: 'approved',
   source_type: "audio",
+  thumbnail: "",
+  tags: [""],
   transcript: "For the remainder of today, we're looking at mostly dry conditions with overcast skies here in Oakwood. However, starting tomorrow, temperatures are set to drop, and we should expect a fair bit of snow accumulation. So if you're heading out, don't forget to bundle up, a warm coat will be essential. Moving into next week, there are signs that warmer weather might finally arrive, hinting that spring is just around the corner. Now, let's shift gears and talk about our local sports scene, including an exciting win from our basketball team last night!",
   audio_url: 'https://res.cloudinary.com/dgi1g967z/video/upload/v1777003937/shadowings/vjs4hqi95yitj51jwgay.mp3',
   duration: 41,
@@ -62,60 +67,70 @@ const mockShadowingData: ShadowingData = {
       translationVi: 'Trong phần còn lại của ngày hôm nay, chúng tôi đang xem xét điều kiện chủ yếu khô ráo với bầu trời u ám ở Oakwood.',
       startTime: 0,
       endTime: 6.39,
+      ipa: ""
     },
     {
       text: 'However, starting tomorrow, temperatures are set to drop,',
       translationVi: 'Tuy nhiên, bắt đầu từ ngày mai, nhiệt độ sẽ giảm xuống,',
       startTime: 6.71,
       endTime: 10.77,
+      ipa: ""
     },
     {
       text: 'and we should expect a fair bit of snow accumulation.',
       translationVi: 'và chúng ta nên mong đợi một chút tuyết tích tụ.',
       startTime: 10.77,
       endTime: 14.18,
+      ipa: ""
     },
     {
       text: "So if you're heading out, don't forget to bundle up,",
       translationVi: 'Vì vậy, nếu bạn đang đi ra ngoài, đừng quên bó lại,',
       startTime: 14.45,
       endTime: 18.15,
+      ipa: ""
     },
     {
       text: 'a warm coat will be essential.',
       translationVi: 'Một chiếc áo khoác ấm sẽ là điều cần thiết.',
       startTime: 18.27,
       endTime: 20.72,
+      ipa: ""
     },
     {
       text: 'Moving into next week,',
       translationVi: 'Chuyển sang tuần tới,',
       startTime: 21.26,
       endTime: 22.8,
+      ipa: ""
     },
     {
       text: 'there are signs that warmer weather might finally arrive,',
       translationVi: 'có những dấu hiệu cho thấy thời tiết ấm hơn cuối cùng cũng có thể đến,',
       startTime: 22.8,
       endTime: 26.28,
+      ipa: ""
     },
     {
       text: 'hinting that spring is just around the corner.',
       translationVi: 'gợi ý rằng mùa xuân đang đến gần.',
       startTime: 26.28,
       endTime: 29.38,
+      ipa: ""
     },
     {
       text: "Now, let's shift gears and talk about our local sports scene,",
       translationVi: 'Bây giờ, hãy chuyển hướng và nói về bối cảnh thể thao địa phương của chúng ta,',
       startTime: 29.49,
       endTime: 34.2,
+      ipa: ""
     },
     {
       text: 'including an exciting win from our basketball team last night!',
       translationVi: 'bao gồm cả chiến thắng thú vị từ đội bóng rổ của chúng tôi đêm qua!',
       startTime: 34.2,
       endTime: 38.26,
+      ipa: ""
     },
   ],
 };

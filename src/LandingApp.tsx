b0b1761885c559f8db9561a2c6d1736ae392c8b1
thanha17 <148538593+thanha17@users.noreply.github.com>
2026-05-20 13:48:52 +0700

    LandingApp.tsx

--- src/LandingApp.tsx
+++ src/LandingApp.tsx
@@ -1,7 +1,6 @@
-import LandingPage from './views/pages/LandingPage';
+import LandingPage from "./views/pages/LandingPage";
+import LandingPageV2 from "./views/pages/LandingPageV2";
 
 export default function LandingApp() {
-    return (
-        <LandingPage />
-    );
+  return <LandingPageV2 />;
 }
